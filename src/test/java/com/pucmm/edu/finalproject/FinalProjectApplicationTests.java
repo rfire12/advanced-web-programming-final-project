ac798455799f7ebd2b8dce16d94bef4d0b0a87f5
José Gabriel González <jgg032899@gmail.com>
2020-08-20 19:36:47 -0400

    Creating project

---
+++ src/test/java/com/pucmm/edu/finalproject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucmm.edu.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
