1fb9938c96eac727798e72daf818ad81ec801815
José Gabriel González <jgg032899@gmail.com>
2020-08-22 11:34:33 -0400

    Adding Users MicroService

---
+++ user-microservice/src/main/java/com/pucmm/edu/usermicro/UserMicroApplication.java
@@ -0,0 +1,13 @@
+package com.pucmm.edu.usermicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMicroApplication.class, args);
+    }
+
+}
