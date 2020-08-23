a1b4917b6cba881dc125924b2e730f2c7997b58a
José Gabriel González <jgg032899@gmail.com>
2020-08-23 17:30:11 -0400

    Minor changes

--- usermicro/src/main/java/usermicro/Controllers/MyUserController.java
+++ usermicro/src/main/java/usermicro/Controllers/MyUserController.java
@@ -3,9 +3,13 @@ package usermicro.Controllers;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import usermicro.Entities.MyUser;
 import usermicro.Services.MyUserService;
 
 @RestController
