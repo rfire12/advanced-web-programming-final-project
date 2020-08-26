bf769464af7d6570297e4461abedd61c70af05b7
José Gabriel González <jgg032899@gmail.com>
2020-08-26 17:10:11 -0400

    Updating Eureka Server

---
+++ eureka-server/src/test/java/com/pucmm/edu/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucmm.edu.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
